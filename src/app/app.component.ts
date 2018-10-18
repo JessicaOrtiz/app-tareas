ec3bcf8232be0ddc1ec5482e18768ba16b0108a5
JessicaOrtiz <jessica_940802@hotmail.com>
2018-10-18 09:57:29 -0500

    Se agrega tareas, falta eliminarla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-tareas';
+  tareas: Tarea[]; //Arrelo de tareas
+  ultimoId: number;
+
+  constructor(){
+    this.tareas = [{id: 1, titulo: 'terminar el API', completada: false},{id: 2, titulo: 'Corregir historias de usuario', completada: true}, {id: 3, titulo: 'Comprar boletos para el cine', completada: false}];
+    this.ultimoId = 3;
+  }
+
+  agreagarTarea(tituloTarea: string){
+    const tareaNueva = new Tarea({titulo: tituloTarea});
+    tareaNueva.id = ++this.ultimoId;
+    this.tareas.push(tareaNueva);
+  }
 }
+
+class Tarea{
+  id: number;
+  titulo: string;
+  completada: boolean;
+
+  constructor(valores: Object = {}){
+    Object.assign(this, valores);
+  }
+}
+
